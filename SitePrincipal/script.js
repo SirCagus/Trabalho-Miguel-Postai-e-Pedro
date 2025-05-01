4754c82ae51cf6194da4b4a17068442c6e4c302a
SirCagus <141450259+SirCagus@users.noreply.github.com>
2025-05-01 17:05:56 -0300

    Limpeza

--- SitePrincipal/script.js
+++
@@ -1,6 +0,0 @@
-const hamburguer = document.getElementById('hamburguer');
-const menu = document.getElementById('menu');
-
-hamburguer.addEventListener('click', function() {
-    menu.classList.toggle('ativo');
-});
